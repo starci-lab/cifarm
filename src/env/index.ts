b47f0f8f70eb1f86e52a63be14c993184998e76b
Le Quoc Uy <lequocuyit@gmail.com>
2025-02-02 16:15:35 +0700

    update env configuration for API and IO URLs

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,5 +1,5 @@
 export const envConfig = () => ({
-    restApiUrl: process.env.NEXT_PUBLIC_REST_API_URL || "http://localhost:3001/api/v1/",
+    restApiUrl: process.env.NEXT_PUBLIC_REST_API_URL || "http://localhost:3001/api",
     graphqlUrl: process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:3006/graphql",
-    ioUrl: process.env.NEXT_PUBLIC_IO_URL || "http://localhost:3003",
+    ioUrl: process.env.NEXT_PUBLIC_IO_URL || "ws://localhost:3003",
 })
