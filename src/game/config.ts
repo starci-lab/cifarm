3b1e4541f4d35584473278caeda83c930ddb6448
starci183 <cuongnvtse160875@gmail.com>
2025-03-07 06:34:44 +0700

    213

--- src/game/config.ts
+++ src/game/config.ts
@@ -7,6 +7,7 @@ import UIPlugin from "phaser3-rex-plugins/templates/ui/ui-plugin.js"
 import { DataScene } from "./scenes/DataScene"
 import CircleMaskImagePlugin from "phaser3-rex-plugins/plugins/circlemaskimage-plugin.js"
 import { UserSchema } from "@/modules/entities"
+import { SoundScene } from "./scenes/SoundScene"
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
@@ -52,7 +53,7 @@ const config: Phaser.Types.Core.GameConfig = {
             }
         ],
     },
-    scene: [BootstrapScene, LoadingScene, GameplayScene, UIScene, DataScene],
+    scene: [BootstrapScene, SoundScene, LoadingScene, GameplayScene, UIScene, DataScene],
 }
 
 export const startGame = (parent?: string) => {
