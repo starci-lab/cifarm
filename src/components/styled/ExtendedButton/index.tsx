9c71cc8e5f217e634796146f6895d1d6efd4abf0
uydev <lequocuyit@gmail.com>
2025-05-11 23:31:54 +0700

    Refactor ExtendedButton component to update pulse effect styling. Change pulse background from rounded-full to rounded-lg for improved visual consistency.

--- src/components/styled/ExtendedButton/index.tsx
+++ src/components/styled/ExtendedButton/index.tsx
@@ -102,11 +102,11 @@ export const ExtendedButton = ({
                 {isLoading && <Spinner />}
                 {children}
             </div>
-            {/* {
+            {
                 pulseColor && (
-                    <div className="absolute left-1/2 top-1/2 size-full -translate-x-1/2 -translate-y-1/2 animate-pulse rounded-full bg-inherit" />
+                    <div className="absolute left-1/2 top-1/2 size-full -translate-x-1/2 -translate-y-1/2 animate-pulse rounded-lg bg-inherit" />
                 )
-            } */}
+            }
         </Button>
     )
 }
