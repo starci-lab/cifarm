8fe8fe0d17529775931e07e236cdf0defadb0a95
uydev <lequocuyit@gmail.com>
2025-05-16 21:25:25 +0700

    Add backdrop blur effect to Navbar for improved visual aesthetics

--- src/app/home/Navbar/index.tsx
+++ src/app/home/Navbar/index.tsx
@@ -13,6 +13,7 @@ export const Navbar: FC = () => {
 
     return (
         <nav className="w-full py-4 z-50 sticky top-0 px-6 max-w-[1200px] mx-auto">
+            <div className="absolute inset-0 bg-opacity-95 backdrop-blur-sm z-0"></div>
             <div className="flex items-center justify-between w-full">
                 <div/>
                 <div className="flex items-center gap-2">
