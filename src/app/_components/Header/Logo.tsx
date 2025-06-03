4b1e8b2941cd33dbfedfbb3cd1a2bcd8ba9adca3
uydev <lequocuyit@gmail.com>
2025-06-03 22:42:58 +0700

    Enhance UI with new Announcement component and shimmer animation; update dependencies in package-lock.json

--- src/app/_components/Header/Logo.tsx
+++ src/app/_components/Header/Logo.tsx
@@ -12,13 +12,15 @@ export const Logo = ({
     const router = useRouterWithSearchParams()
 
     return (
-        <Link classNames={{ base: "flex items-center gap-2" }}
-            onClick={() => {
-                router.push("/")
-            }}
-        >
-            <Image src="https://cifarm.sgp1.cdn.digitaloceanspaces.com/logo.png" alt="Cifarm Logo" className="h-auto w-10 h-10 rounded-full" />
-            {withText && <p className="text-muted-foreground text-2xl">CiFarm</p>}
-        </Link>
+        <div className="transition-all hover:duration-1000 motion-reduce:hover:transition-none [mask-image:linear-gradient(60deg,#000_25%,rgba(0,0,0,.2)_50%,#000_75%)] [mask-position:0] [mask-size:400%] hover:[mask-position:100%]">
+            <Link classNames={{ base: "flex items-center gap-2" }}
+                onClick={() => {
+                    router.push("/")
+                }}
+            >
+                <Image src="https://cifarm.sgp1.cdn.digitaloceanspaces.com/logo.png" alt="Cifarm Logo" className="h-auto w-10 h-10 rounded-full" />
+                {withText && <p className="text-muted-foreground text-2xl">CiFarm</p>}
+            </Link>
+        </div>
     )
 } 
--- src/app/_components/Hero/AnnouncementBanner.tsx
+++ src/app/_components/Hero/AnnouncementBanner.tsx
@@ -3,11 +3,14 @@ import React from "react"
 
 interface AnnouncementBannerProps {
     text: string
+    link: string
 }
 
-export const AnnouncementBanner = ({ text }: AnnouncementBannerProps) => (
-    <div className="flex items-center space-x-2 bg-content-2 w-fit px-3 py-1.5 rounded-full">
-        <span className="text-secondary text-sm font-semibold">{text}</span>
-        <ArrowRight className="h-4 w-4 text-secondary" />
-    </div>
-) 
+export const AnnouncementBanner = ({ text, link }: AnnouncementBannerProps) => {
+    return (
+        <a className="flex items-center space-x-2 bg-content-2 w-fit px-3 py-1.5 rounded-full" href={link} target="_blank" rel="noreferrer">
+            <span className="text-secondary text-sm font-semibold">{text}</span>
+            <ArrowRight className="h-4 w-4 text-secondary" />
+        </a>
+    )
+} 
